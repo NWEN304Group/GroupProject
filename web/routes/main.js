dcc9da520fc14952ef2be086d4ea986d3bcfcd01
(Alex) Zhen Wang <wangzhennz@gmail.com>
2016-06-17 03:57:14 +1200

    finish pagination

--- web/routes/main.js
+++ web/routes/main.js
@@ -1,8 +1,44 @@
 var router = require('express').Router();
 var product = require('../product/product')
 
-router.get('/', function (req, res) {
-    res.render('main/index');
+router.get('/', function (req, res, next) {
+    if (req.user) {
+        var productsInOnePage = 6;
+        var page = req.params.page;
+        product.find()
+            .skip(productsInOnePage * (page-1))
+            .limit(productsInOnePage)
+            .populate('category')
+            .exec(function (err, products) {
+                if (err) return next(err);
+                product.count().exec(function (err, count) {
+                    if (err) return next(err);
+                    var num = Math.ceil(count / productsInOnePage);
+                    res.render('product/productsHomePage', {
+                        products: products,
+                        numOfPage: num
+                    });
+                });
+            });
+    } else {
+        var productsInOnePage = 6;
+        var page = req.params.page;
+        product.find()
+            .skip(productsInOnePage * (page-1))
+            .limit(productsInOnePage)
+            .populate('category')
+            .exec(function (err, products) {
+                if (err) return next(err);
+                product.count().exec(function (err, count) {
+                    if (err) return next(err);
+                    var num = Math.ceil(count / productsInOnePage);
+                    res.render('main/index', {
+                        products: products,
+                        numOfPage: num
+                    });
+                });
+            });
+    }
 });
 
 //get products from id of one category
@@ -21,13 +57,53 @@ router.get('/products/:id', function (req, res, next) {
 // test id 57623034ec93478e42015cfb
 
 //get a product by id
-router.get('/product/:id', function(req, res, next) {
-    product.findById({ _id: req.params.id }, function(err, productFound) {
+router.get('/product/:id', function (req, res, next) {
+    product.findById({_id: req.params.id}, function (err, productFound) {
         if (err) return next(err);
         res.render('product/oneProductPage', {
             productFound: productFound
         });
     });
+});
+
+router.get('/page/:page', function (req, res, next) {
+    var productsInOnePage = 6;
+    var page = req.params.page;
+    product.find()
+        .skip(productsInOnePage * (page-1))
+        .limit(productsInOnePage)
+        .populate('category')
+        .exec(function (err, products) {
+            if (err) return next(err);
+            product.count().exec(function (err, count) {
+                if (err) return next(err);
+                var num = Math.ceil(count / productsInOnePage);
+                res.render('product/productsHomePage', {
+                    products: products,
+                    numOfPage: num
+                });
+            });
+        });
+});
+
+router.get('/pagenotlogin/:page', function (req, res, next) {
+    var productsInOnePage = 6;
+    var page = req.params.page;
+    product.find()
+        .skip(productsInOnePage * (page-1))
+        .limit(productsInOnePage)
+        .populate('category')
+        .exec(function (err, products) {
+            if (err) return next(err);
+            product.count().exec(function (err, count) {
+                if (err) return next(err);
+                var num = Math.ceil(count / productsInOnePage);
+                res.render('main/index', {
+                    products: products,
+                    numOfPage: num
+                });
+            });
+        });
 });
 
 
