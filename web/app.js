b26f2a5fea607fc9eab72ad0adbcb46b4e740ddf
(Alex) Zhen Wang <wangzhennz@gmail.com>
2016-06-16 15:05:59 +1200

    add category and product

--- web/app.js
+++ web/app.js
@@ -12,15 +12,16 @@ var passport = require('passport');
 
 var secret = require('./config/secret');
 var User = require('./models/user');
+var Category = require('./models/category');
 
 var app = express();
 
-mongoose.connect(secret.database, function(error){
-	if(error){
-		console.log(error);
-	}else{
-		console.log('mongodb connected');
-	}
+mongoose.connect(secret.database, function (error) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('mongodb connected');
+    }
 });
 
 // Add middleware
@@ -28,15 +29,15 @@ app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
 app.use(cookieParser());
 
 app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: secret.secretKey,
-  store: new MongoStore({ url: secret.database, autoReconnect: true})
+    resave: true,
+    saveUninitialized: true,
+    secret: secret.secretKey,
+    store: new MongoStore({url: secret.database, autoReconnect: true})
 }));
 
 app.use(flash());
@@ -44,9 +45,9 @@ app.use(flash());
 app.use(passport.initialize());
 // serialize and deserialize
 app.use(passport.session());
-app.use(function(req, res, next) {
-  res.locals.user = req.user;
-  next();
+app.use(function (req, res, next) {
+    res.locals.user = req.user;
+    next();
 });
 
 app.engine('ejs', ejsMate);
@@ -59,7 +60,7 @@ var userRoute = require('./routes/user');
 app.use(mainRoute);
 app.use(userRoute);
 
-app.listen(secret.port, function(error){
-	if (error) throw error;
-	console.log("Server is listenning on port " + secret.port);
+app.listen(secret.port, function (error) {
+    if (error) throw error;
+    console.log("Server is listenning on port " + secret.port);
 });
--- web/config/secret.js
+++ web/config/secret.js
@@ -1,6 +1,7 @@
 module.exports = {
 
-  database: 'mongodb://root:qw12er34@ds019053.mlab.com:19053/nwen304test',
+  // database: 'mongodb://root:qw12er34@ds019053.mlab.com:19053/nwen304test',
+  database:'mongodb://ZhenWang:1991gaokao2009@ds015934.mlab.com:15934/newn304groupproject',
   port: 8080,
   secretKey: "qw12er34"
 }
