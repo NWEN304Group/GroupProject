7d3f2560911682f1ba71bcbbc524935fa925f02e
(Alex) Zhen Wang <wangzhennz@gmail.com>
2016-06-18 16:18:36 +1200

    prohibit user to modify database

--- routes/operateDB.js
+++ routes/operateDB.js
@@ -8,22 +8,22 @@
 var router = require('express').Router();
 var Category = require('../product/category');
 
-//return add category page
-router.get('/addCategory', function(req, res, next) {
-    res.render('operateDB/addCategoryPage', { message: req.flash('success') });
-});
-
-//add to database
-router.post('/addCategory', function(req, res, next) {
-    var category = new Category();
-    category.name = req.body.nameInput;
-
-    category.save(function(err) {
-        if (err) return next(err);
-        req.flash('success', 'Adding category success');
-        return res.redirect('/addCategory');
-    });
-})
+// //return add category page
+// router.get('/addCategory', function(req, res, next) {
+//     res.render('operateDB/addCategoryPage', { message: req.flash('success') });
+// });
+//
+// //add to database
+// router.post('/addCategory', function(req, res, next) {
+//     var category = new Category();
+//     category.name = req.body.nameInput;
+//
+//     category.save(function(err) {
+//         if (err) return next(err);
+//         req.flash('success', 'Adding category success');
+//         return res.redirect('/addCategory');
+//     });
+// })
 
 module.exports = router;
 
