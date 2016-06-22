8801130fe8774433c1444f4dd65e221d428226fb
Colton Vermilyea <vermilcolt@myvuw.ac.nz>
2016-06-22 17:59:35 +1200

    Make callbackURL relative

--- config/secret.js
+++ config/secret.js
@@ -9,7 +9,6 @@ module.exports = {
   	clientID: process.env.FACEBOOK_ID || '188826648185616',
   	clientSecret: process.env.FACEBOOK_SECRET || 'a158b766ecdb6181d123a6ea4f3b6fb0',
   	profileFields: ['emails', 'displayName'],
-  	callbackURL: 'https://nwen304-shopping-project.herokuapp.com/auth/facebook/callback'
-    // callbackURL: 'http://localhost:8080/auth/facebook/callback'
+  	callbackURL: '/auth/facebook/callback'
   }
 }
